a677672c68a0d0edeae38aaf4aa1ad50969efd90
MakerSquare Student <students@themakersquare.com>
2013-07-10 17:45:29 -0500

    Edited Home Controller, added private method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,32 @@
 class HomeController < ApplicationController
   def index
     @response = Instagram.media_popular
+    # @response = recent_instagram_images("30.2669","-97.7428")
     @instagrams = @response.map do |item|
       item.images.low_resolution.url
     end
   end
-  #   counter = 0
-  #   response = nil
-  #   while counter < 5 && !response
-  #     begin
-  #       response = Instagram.media_search("30.2669","-97.7428")
-  #       puts counter += 1
-  #     rescue Instagram::BadRequest => e
-  #       e.inspect
-  #     end
-  #   end
-  #   @instagrams = response.map do |item|
-  #     item.images.low_resolution.url
-  #   end
-  # end
+ 
+
+
+
+ 
+ private
+
+ def recent_instagram_images(latitude,longitude)
+  counter = 0
+  response = nil
+  while counter < 5 && !response
+    begin
+      response = Instagram.media_search(latitude,longitude)
+      puts counter += 1
+    rescue Instagram::BadRequest => e
+      e.inspect
+    end
+  end
+  return response.map do |item|
+    item.images.low_resolution.url
+  end
+end
+
 end
